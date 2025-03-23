537a18f647e8d55ed41d491a8cc2bc82e4aa8640
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-03-23 20:26:53 +0100

    feat: model relations

--- app/Models/Desk.php
+++ app/Models/Desk.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Desk extends Model
 {
     /** @use HasFactory<\Database\Factories\DeskFactory> */
     use HasFactory;
+
+
+    public function reservations(): HasMany {
+        return $this->hasMany(Reservation::class);
+    }
 }
--- app/Models/Office.php
+++ app/Models/Office.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Office extends Model
 {
     /** @use HasFactory<\Database\Factories\OfficeFactory> */
     use HasFactory;
+
+    public function desks(): HasMany
+    {
+        return $this->HasMany(Desk::class);
+    }
 }
