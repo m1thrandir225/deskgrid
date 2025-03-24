fe7e30d3334c6c4fae0967a21a1594aaa2fee105
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-03-24 20:20:54 +0100

    feat: desk controller boiler plate & .gitignore updated

--- app/Http/Controllers/DeskController.php
+++ app/Http/Controllers/DeskController.php
@@ -2,9 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Desk;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class DeskController extends Controller
 {
-    //
+    /**
+    * Show the create desk page.
+    */
+    public function create(Request $request): Response
+    {
+        return Inertia::render('desk/create', [
+        ]);
+    }
+
+    /**
+    * Handle an incoming new desk request
+    *
+    * @throws \Illuminate\Validation\ValidationException
+    */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+
+        ]);
+
+        $desk = Desk::create([
+
+        ]);
+
+        return to_route('dashboard');
+    }
+
+    /**
+    * Show the current desk
+    * -> should include all reservations
+    */
+    public function show(Request $request)
+    {
+
+    }
+
+    /*
+    * Show the edit form
+    */
+    public function edit(Request $request)
+    {
+    }
+
+    /*
+    * Handle an edit request for the resource
+    */
+
+    public function update(Request $request)
+    {
+    }
+
+    /*
+    * Show the destroy page
+    * TODO: might not be needed
+    */
+    public function destroy()
+    {
+    }
+
+    /*
+    * Handle a elete request
+    */
+    public function delete()
+    {
+    }
 }
