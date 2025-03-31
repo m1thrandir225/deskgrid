0be08ecde71970e82d838550e4f4ba319cc23183
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-03-31 20:23:41 +0200

    feat: floor policy & controller actions

--- app/Http/Controllers/FloorController.php
+++ app/Http/Controllers/FloorController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Floor\CreateFloorRequest;
+use App\Http\Requests\Floor\UpdateFloorRequest;
 use App\Models\Floor;
+use App\Models\Office;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -21,16 +25,31 @@ class FloorController extends Controller
 
     public function create(Request $request): Response
     {
+        Gate::authorize('create', Floor::class);
+
         return Inertia::render('floors/create');
     }
 
-    public function store(Request $request): RedirectResponse
+    public function store(CreateFloorRequest $request): RedirectResponse
     {
+        Gate::authorize('create', Floor::class);
+
+        $validated = $request -> validated();
+
+        //TODO: implement file upload
+
+        Floor::create([
+            'office_id' => $validated['office_id'],
+            'name' => $validated['name'],
+            'plan_image' => $validated['plan_image']
+        ]);
         return to_route('floors.index');
     }
 
     public function show(Floor $floor): Response
     {
+        Gate::authorize('view', $floor);
+
         return Inertia::render('floors/show', [
             "floor" => $floor
         ]);
@@ -38,18 +57,33 @@ class FloorController extends Controller
 
     public function edit(Floor $floor): Response
     {
+        Gate::authorize('update', $floor);
+
         return Inertia::render('floors/edit', [
             "floor" => $floor,
         ]);
     }
 
-    public function update(Request $request, Floor $floor): RedirectResponse
+    public function update(UpdateFloorRequest $request, Floor $floor): RedirectResponse
     {
-        return to_route('floors.show', $floor);
+        Gate::authorize('update', $floor);
+
+        $validated = $request->validated();
+
+        $floor->update([
+            'name' => $validated['name'],
+            'plan_image' => $validated['plan_image']
+        ]);
+
+        return to_route('floors.show', $floor->id);
     }
 
     public function delete(Floor $floor): RedirectResponse
     {
+        Gate::authorize('delete', $floor);
+
+        $floor->delete();
+
         return to_route('floors.index');
     }
 }
---
+++ app/Policies/FloorPolicy.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Policies;
+
+use App\Enums\UserRole;
+use App\Models\Floor;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class FloorPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Floor $floor): bool
+    {
+        $floor_user_id = $floor->office()->employer()->id;
+        return $user->id === $floor_user_id;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->role === UserRole::Admin;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Floor $floor): bool
+    {
+        $floor_user_id = $floor->office()->employer()->id;
+
+        return $user->id === $floor_user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Floor $floor): bool
+    {
+        $floor_user_id = $floor->office()->employer()->id;
+
+        return $user->id === $floor_user_id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Floor $floor): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Floor $floor): bool
+    {
+        return false;
+    }
+}
